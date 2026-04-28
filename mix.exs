1f2ac60b3da196f530eaa69bb5ac4f1a4bbf871e
GenericJam <GenericJam@gmail.com>
2026-04-28 03:54:30 -0600

    0.1.21

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MobNew.MixProject do
   def project do
     [
       app: :mob_new,
-      version: "0.1.20",
+      version: "0.1.21",
       elixir: "~> 1.19",
       deps: deps(),
       description: "Project generator for the Mob mobile framework",
